c3e2d3e0da971afccf37fbff65a88148e562a87b
sjmarsh <sjmarsh31@gmail.com>
2017-01-09 16:16:29 +1100

    Add Gulp production config
    Minifiy only for Production.  Still names files with min.

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,20 @@ var concatCSS = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 var imagemin = require('gulp-imagemin');
 var gulpNgConfig = require('gulp-ng-config');
+var util = require('gulp-util');
 
-var scriptsPath = 'app';
-var viewsPath = 'app/views';
-var stylesPath = 'assets/css';
-var imagesPath = 'assets/img';
-var libPath = 'assets/lib';
-var outputPath = 'dist';
+var config = {
+  scriptsPath : 'app',
+  viewsPath : 'app/views',
+  stylesPath : 'assets/css',
+  imagesPath : 'assets/img',
+  libPath : 'assets/lib',
+  outputPath : 'dist',
+  production: !!util.env.production
+};
 
-var config = JSON.parse(fs.readFileSync('c:\\aws\\awsaccess.json'));
-var s3 = require('gulp-s3-upload')(config);
+var awsConfig = JSON.parse(fs.readFileSync('c:\\aws\\awsaccess.json'));
+var s3 = require('gulp-s3-upload')(awsConfig);
 
 /*
 Ref: https://github.com/gulpjs/gulp/tree/master/docs/recipes
@@ -35,99 +39,92 @@ function getFolders(dir) {
 
 gulp.task('clean', function () {
   return del([
-    outputPath + '/**/*'
+    config.outputPath + '/**/*'
   ]);
 });
 
 gulp.task('index', ['clean'], function () {
   return gulp.src('./index.html')
-    .pipe(gulp.dest(outputPath));
+    .pipe(gulp.dest(config.outputPath));
 });
 
 gulp.task('views', ['clean'], function () {
-  return gulp.src(viewsPath + '/*.html')
-    .pipe(gulp.dest(outputPath + '/views'));
+  return gulp.src(config.viewsPath + '/*.html')
+    .pipe(gulp.dest(config.outputPath + '/views'));
 });
 
 gulp.task('scripts', ['clean'], function () {
-  var folders = getFolders(scriptsPath);
+  var folders = getFolders(config.scriptsPath);
 
   var tasks = folders.map(function (folder) {
-    return gulp.src(path.join(scriptsPath, folder, '/**/*.js'))
+    return gulp.src(path.join(config.scriptsPath, folder, '/**/*.js'))
       // concat into foldername.js
       .pipe(concat(folder + '.js'))
-      // write to output
-     // .pipe(gulp.dest(outputPath))
-      // minify
-      .pipe(uglify())
+      // minify if required
+      .pipe(config.production ? uglify() : util.noop())
       // rename to folder.min.js
       .pipe(rename(folder + '.min.js'))
       // write to output again
-      .pipe(gulp.dest(outputPath));
+      .pipe(gulp.dest(config.outputPath));
   });
 
-  // process all remaining files in scriptsPath root into main.js and main.min.js files
-  var root = gulp.src(path.join(scriptsPath, '/*.js'))
+  // process all remaining files in config.scriptsPath root into main.min.js file
+  var root = gulp.src(path.join(config.scriptsPath, '/*.js'))
        .pipe(concat('main.js'))
-       //.pipe(gulp.dest(outputPath))
-       .pipe(uglify())
+       .pipe(config.production ? uglify() : util.noop())
        .pipe(rename('main.min.js'))
-       .pipe(gulp.dest(outputPath));
+       .pipe(gulp.dest(config.outputPath));
 
   return merge(tasks, root);
 });
 
 gulp.task('styles', ['clean'], function () {
-  return gulp.src(stylesPath + '/*.css')
+  return gulp.src(config.stylesPath + '/*.css')
     .pipe(concatCSS('styles.min.css'))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest(outputPath + '/css'));
+    .pipe(config.production ? cleanCSS() : util.noop())
+    .pipe(gulp.dest(config.outputPath + '/css'));
 });
 
 gulp.task('images', ['clean'], function () {
-  return gulp.src(imagesPath + '/*')
+  return gulp.src(config.imagesPath + '/*')
         .pipe(imagemin())
-        .pipe(gulp.dest(outputPath + '/img'));
+        .pipe(gulp.dest(config.outputPath + '/img'));
 });
 
 gulp.task('lib', ['clean'], function () {
-  var folders = getFolders(libPath);
+  var folders = getFolders(config.libPath);
 
   var tasks = folders.map(function (folder) {
-    return gulp.src(path.join(libPath, folder, '/**/*.js'))
+    return gulp.src(path.join(config.libPath, folder, '/**/*.js'))
       // concat into foldername.js
       .pipe(concat(folder + '.js'))
-      // write to output
-     // .pipe(gulp.dest(outputPath))
-      // minify
-      .pipe(uglify())
+      .pipe(config.production ? uglify() : util.noop)
       // rename to folder.min.js
       .pipe(rename(folder + '.min.js'))
       // write to output again
-      .pipe(gulp.dest(outputPath));
+      .pipe(gulp.dest(config.outputPath));
   });
 
-  // process all remaining files in libPath root into lib.js and lib.min.js files
-  var root = gulp.src(path.join(libPath, '/*.js'))
+  // process all remaining files in config.libPath root into lib.js and lib.min.js files
+  var root = gulp.src(path.join(config.libPath, '/*.js'))
        .pipe(concat('lib.js'))
-       //.pipe(gulp.dest(outputPath))
-       .pipe(uglify())
+       .pipe(config.production ? uglify() : util.noop())
        .pipe(rename('lib.min.js'))
-       .pipe(gulp.dest(outputPath));
+       .pipe(gulp.dest(config.outputPath));
 
   return merge(tasks, root);
 });
 
 gulp.task('favicon', ['clean'], function () {
   return gulp.src('./favicon.ico')
-    .pipe(gulp.dest(outputPath));
+    .pipe(gulp.dest(config.outputPath));
 });
 
 /*
 Ref: https://www.npmjs.com/package/gulp-s3-upload
      http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-json-file.html
 */
-gulp.task('publish', ['prod'], function() {
+gulp.task('publish', function() {
   return gulp.src("./dist/**")
         .pipe(s3({
             Bucket: 'movies-angular1',  
@@ -138,23 +135,12 @@ gulp.task('publish', ['prod'], function() {
     ;
 });
 
-gulp.task('applyLocalConfig', function() {
+gulp.task('applyConfig', function() {
   return gulp.src('./app/config.json')
     .pipe(gulpNgConfig('MoviesApp.config', {
-      environment: 'local'
+      environment: config.production ? 'production' : 'local'
     }))
     .pipe(gulp.dest('./app/'));
 });
 
-gulp.task('applyProdConfig', function() {
-  return gulp.src('./app/config.json')
-    .pipe(gulpNgConfig('MoviesApp.config', {
-      environment: 'production'
-    }))
-    .pipe(gulp.dest('./app/'));
-});
-
-
-gulp.task('default', ['applyLocalConfig', 'index', 'views', 'scripts','styles', 'images', 'lib', 'favicon']);
-
-gulp.task('prod', ['applyProdConfig', 'index', 'views', 'scripts','styles', 'images', 'lib', 'favicon']);
+gulp.task('default', ['applyConfig', 'index', 'views', 'scripts','styles', 'images', 'lib', 'favicon']);
