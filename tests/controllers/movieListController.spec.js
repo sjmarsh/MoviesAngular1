4ee959a9343c4368edb4df3538af4c2c57ff8ab5
sjmarsh <sjmarsh31@gmail.com>
2017-01-20 12:47:50 +1100

    Add more tests for movieListController

--- tests/controllers/movieListController.spec.js
+++ tests/controllers/movieListController.spec.js
@@ -1,12 +1,15 @@
 var expect = chai.expect;
 
-describe.only('movieListController', function(){
+describe('movieListController', function(){
     
-    var scope, location, routeParams, toastr, fakeMovieService, fakeMovieSearchService, fakeReferenceDataService, q, deferredMovies, deferredCategories;
-    var getCategoriesSpy, getMoviesSpy;
+    var scope, location, routeParams, toastr, fakeMovieService, fakeMovieSearchService, fakeReferenceDataService, q, deferredMovies, deferredCategories, deferredMoviesByQuery;
+    var getCategoriesSpy, getMoviesSpy, getMoviesByQuerySpy;
 
     var lastSearchFilter = 'super';
     var lastSelectedCategories = ['drama', 'music'];
+    var hasMorePages = false;
+    var nextSkipPage = 11;
+    var nextPageTakeSize = 10;
 
     beforeEach(function(){
       
@@ -16,10 +19,15 @@ describe.only('movieListController', function(){
           getMovies: function(skip, take){
             deferredMovies = q.defer();
             return deferredMovies.promise;
+          },
+          getMoviesByQuery: function(searchFilter, categories, skip, take){
+            deferredMoviesByQuery = q.defer();
+            return deferredMoviesByQuery.promise;
           }
       };
 
       getMoviesSpy = sinon.spy(fakeMovieService,'getMovies');
+      getMoviesByQuerySpy = sinon.spy(fakeMovieService, 'getMoviesByQuery');
 
       fakeReferenceDataService = {
         getCategories: function(){
@@ -31,7 +39,7 @@ describe.only('movieListController', function(){
       getCategoriesSpy = sinon.spy(fakeReferenceDataService, 'getCategories');
 
       fakeMovieSearchService = {
-        storeLastSearchFilter: sinon.stub(),
+        storeLastSearchFilter: sinon.spy(),
         getLastSearchFilter : sinon.stub(),
         storeLastSelectedCategories : sinon.stub(),
         getLastSelectedCategories : sinon.stub(),
@@ -39,9 +47,12 @@ describe.only('movieListController', function(){
         getStoredResults : sinon.stub(),
         hasStoredResults : sinon.stub(),       
         currentPage : sinon.stub(),
-        hasMorePages : sinon.stub(),
-        nextPageSkip : sinon.stub(),
-        nextPageTakeSize : sinon.stub(),
+        incrementPage : sinon.spy(),
+        resetCurrentPage : sinon.spy(),
+        fakeHasMorePages: function(_hasMorePages) { hasMorePages = _hasMorePages; },
+        hasMorePages : function() { return hasMorePages; },
+        nextPageSkip : function() { return nextSkipPage },
+        nextPageTakeSize :  function() { return nextPageTakeSize },
         isSearchPanelVisible : sinon.stub()
       };
 
@@ -70,38 +81,123 @@ describe.only('movieListController', function(){
     expect(scope.pageClass).to.be.equal('page-movie-list');
   });
 
-  it('shouldGetCategories', function(){
-    expect(getCategoriesSpy.calledOnce).to.be.true;
+  describe('#initialize', function(){
+    
+    it('shouldGetCategories', function(){
+      expect(getCategoriesSpy.calledOnce).to.be.true;
+    });
+
+    it('shouldPopulateCategoryList', function(){
+      var categoryResponse = {data: ['action', 'comedy']};
+      deferredCategories.resolve(categoryResponse);
+      scope.$apply();
+      expect(scope.categoryList).to.be.equal(categoryResponse.data);
+    });
+
+    it('shouldPopulateSearchFilterWithLastSearchFilterIfExists', function(){    
+      expect(scope.searchFilter).to.be.equal(lastSearchFilter);
+    });
+
+    it('shouldPopulateLastSelectedCategoriesIfExists', function(){
+      expect(scope.selectedCategories).to.be.equal(lastSelectedCategories);
+    });
+
+    it('shouldSetSearchPanelVisibility', function(){
+      expect(scope.isSearchPanelVisible).to.be.true;
+    });
+
+    it('shouldGetMovies', function(){
+      expect(getMoviesSpy.calledOnce).to.be.true;
+    });
+
+    it('shouldPopulateMovieListWithInitialResults', function(){
+      var moviesResponse = { data: { movies: [{ title:'super', category:'action'}, { title:'garfield', category:'animation'} ]}};
+      deferredMovies.resolve(moviesResponse);
+      scope.$apply();
+      expect(scope.movieList).to.be.equal(moviesResponse.data.movies);
+    });
+
   });
 
-  it('shouldPopulateCategoryList', function(){
-    var categoryResponse = {data: ['action', 'comedy']};
-    deferredCategories.resolve(categoryResponse);
-    scope.$apply();
-    expect(scope.categoryList).to.be.equal(categoryResponse.data);
+  describe('#getMoreMovies', function(){
+    
+    it('shouldGetMoreMoviesIfHasMorePages', function() {
+      fakeMovieSearchService.fakeHasMorePages(true);
+      scope.searchFilter = 'super';
+      scope.selectedCategories = ['drama', 'horror'];
+
+      scope.getMoreMovies();
+
+      expect(getMoviesByQuerySpy.calledOnce).to.be.true;
+      expect(getMoviesByQuerySpy.calledWithExactly(scope.searchFilter, scope.selectedCategories, nextSkipPage, nextPageTakeSize)).to.be.true;
+    });
+
+    it('shouldNotGetMoreMoviesIfHasNoMorePages', function(){
+      fakeMovieSearchService.fakeHasMorePages(false);
+
+      scope.getMoreMovies();
+
+      expect(getMoviesByQuerySpy.called).to.be.false;
+    });
+
+    it('shouldPopulateMovieListWithResult', function(){
+      fakeMovieSearchService.fakeHasMorePages(true);
+      var moviesResponse = { data: { movies: [{ title:'super', category:'action'}, { title:'garfield', category:'animation'} ]}};
+      
+      scope.getMoreMovies();
+      deferredMoviesByQuery.resolve(moviesResponse);
+      scope.$apply();
+
+      expect(scope.movieList).to.deep.equal(moviesResponse.data.movies);
+    });
+
+    it('shouldIncrementPage', function(){
+      fakeMovieSearchService.fakeHasMorePages(true);
+      var moviesResponse = { data: { movies: [{ title:'super', category:'action'}, { title:'garfield', category:'animation'} ]}};
+      
+      scope.getMoreMovies();
+      deferredMoviesByQuery.resolve(moviesResponse);
+      scope.$apply();
+
+      expect(fakeMovieSearchService.incrementPage.calledOnce).to.be.true;
+    });
   });
 
-  it('shouldPopulateSearchFilterWithLastSearchFilterIfExists', function(){    
-    expect(scope.searchFilter).to.be.equal(lastSearchFilter);
-  });
+  describe('#search', function(){
 
-  it('shouldPopulateLastSelectedCategoriesIfExists', function(){
-    expect(scope.selectedCategories).to.be.equal(lastSelectedCategories);
-  });
+    it('shouldStoreLastSearchFilter', function(){
+      var searchFilter = 'random';
+      scope.searchFilter = searchFilter;
 
-  it('shouldSetSearchPanelVisibility', function(){
-    expect(scope.isSearchPanelVisible).to.be.true;
-  });
+      scope.search();
 
-  it('shouldGetMovies', function(){
-    expect(getMoviesSpy.calledOnce).to.be.true;
-  });
+      expect(fakeMovieSearchService.storeLastSearchFilter.calledWithExactly(searchFilter)).to.be.true;
+    });
 
-  it('shouldPopulateMovieListWithInitialResults', function(){
-    var moviesResponse = { data: { movies: [{ title:'super', category:'action'}, { title:'garfield', category:'animation'} ]}};
-    deferredMovies.resolve(moviesResponse);
-    scope.$apply();
-    expect(scope.movieList).to.be.equal(moviesResponse.data.movies);
-  });
+    it('shouldResetCurrentPage', function(){
+      scope.search();
+      expect(fakeMovieSearchService.resetCurrentPage.calledOnce).to.be.true;
+    });
 
+    it('shouldGetMoviesByQuery', function(){
+      var searchFilter = 'random';
+      scope.searchFilter = searchFilter;
+      var selectedCategories = ['comedy'];
+      scope.selectedCategories = selectedCategories;
+
+      scope.search();
+      expect(fakeMovieService.getMoviesByQuery.calledWithExactly(searchFilter, selectedCategories)).to.be.true;
+    });
+
+    it('shouldStoreQueryResults', function(){
+      var moviesResponse = { data: { movies: [{ title:'super', category:'action'}, { title:'garfield', category:'animation'} ]}};
+
+      scope.search();
+
+      deferredMoviesByQuery.resolve(moviesResponse);
+      scope.$apply();
+
+      expect(fakeMovieSearchService.storeResults.calledWithExactly(moviesResponse.data)).to.be.true;
+    });
+  });
 });
